2610e5153545248f38fcfbf4afabf46ae931bced
jerrita <je5r1ta@icloud.com>
2025-07-17 13:12:03 +0800

    init

--- lib/sbi/objects.mk
+++ lib/sbi/objects.mk
@@ -64,6 +64,9 @@ libsbi-objs-$(CONFIG_SBI_ECALL_SSE) += sbi_ecall_sse.o
 carray-sbi_ecall_exts-$(CONFIG_SBI_ECALL_MPXY) += ecall_mpxy
 libsbi-objs-$(CONFIG_SBI_ECALL_MPXY) += sbi_ecall_mpxy.o
 
+carray-sbi_ecall_exts-$(CONFIG_SBI_ECALL_TRF) += ecall_trf
+libsbi-objs-$(CONFIG_SBI_ECALL_TRF) += sbi_ecall_trf.o
+
 libsbi-objs-y += sbi_bitmap.o
 libsbi-objs-y += sbi_bitops.o
 libsbi-objs-y += sbi_console.o
