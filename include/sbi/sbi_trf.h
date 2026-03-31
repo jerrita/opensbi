4b6e97e43f62406f56ed60f6f277ef21f035e27f
jerrita <je5r1ta@icloud.com>
2026-03-31 12:50:25 +0800

    feat: nginx skeleton

--- include/sbi/sbi_trf.h
+++ include/sbi/sbi_trf.h
@@ -12,6 +12,14 @@ enum trf_func_desc { INITIALIZE = 0, COPY_FROM_NORMAL, FUNC_SBI, FUNC_RS };
 // rs -> sbi
 enum gated_func_desc { OK = 0, TBI_PUT_CHAR, TBI_PUT_STR, TBI_TEST_FUNC };
 
+enum trf_nginx_func_desc {
+	NGINX_PRIVKEY_IMPORT = 10,
+	NGINX_PRIVKEY_SIGN,
+	NGINX_PRIVKEY_DECRYPT,
+	NGINX_PRIVKEY_DESTROY,
+	NGINX_PRIVKEY_GET_PUBKEY,
+};
+
 struct secure_ret {
 	isize func_or_ret;
 	usize a0, a1, a2;
@@ -19,7 +27,7 @@ struct secure_ret {
 
 struct trf_call {
 	usize func_desc;
-	usize a0, a1, a2;
+	usize a0, a1, a2, a3, a4, a5;
 };
 
 int copy_from_normal(char *buf, const char *src, int len);
--- lib/sbi/sbi_ecall_trf.c
+++ lib/sbi/sbi_ecall_trf.c
@@ -14,10 +14,13 @@ struct secure_ret secure_rs_call(struct trf_call sc)
 	struct secure_ret ret;
 	secure_rs_call_t rs_func = (secure_rs_call_t)(_fw_start + RS_OFFSET);
 	__asm__ volatile("mv a0, %4\n\t" // func_desc -> a0
-			 "mv a1, %5\n\t" // a1 -> a1
-			 "mv a2, %6\n\t" // a2 -> a2
-			 "mv a3, %7\n\t" // a3 -> a3
-			 "jalr %8\n\t"	 // 调用函数
+			 "mv a1, %5\n\t" // sc.a0 -> a1
+			 "mv a2, %6\n\t" // sc.a1 -> a2
+			 "mv a3, %7\n\t" // sc.a2 -> a3
+			 "mv a4, %8\n\t" // sc.a3 -> a4
+			 "mv a5, %9\n\t" // sc.a4 -> a5
+			 "mv a6, %10\n\t" // sc.a5 -> a6
+			 "jalr %11\n\t"	 // 调用函数
 			 "mv %0, a0\n\t" // ret.func_or_ret <- a0
 			 "mv %1, a1\n\t" // ret.a1 <- a1
 			 "mv %2, a2\n\t" // ret.a2 <- a2
@@ -25,8 +28,10 @@ struct secure_ret secure_rs_call(struct trf_call sc)
 			 : "=r"(ret.func_or_ret), "=r"(ret.a0), "=r"(ret.a1),
 			   "=r"(ret.a2) // 输出
 			 : "r"(sc.func_desc), "r"(sc.a0), "r"(sc.a1),
-			   "r"(sc.a2), "r"(rs_func)	// 输入
-			 : "a0", "a1", "a2", "a3", "ra" // 被修改的寄存器
+			   "r"(sc.a2), "r"(sc.a3), "r"(sc.a4), "r"(sc.a5),
+			   "r"(rs_func)	// 输入
+			 : "a0", "a1", "a2", "a3", "a4", "a5", "a6",
+			   "ra" // 被修改的寄存器
 	);
 	return ret;
 }
@@ -36,7 +41,10 @@ int copy_from_normal(char *buf, const char *src, int len)
 	struct trf_call sc    = { .func_desc = COPY_FROM_NORMAL,
 				  .a0	     = (usize)buf,
 				  .a1	     = (usize)src,
-				  .a2	     = len };
+				  .a2	     = len,
+				  .a3	     = 0,
+				  .a4	     = 0,
+				  .a5	     = 0 };
 	struct secure_ret ret = secure_rs_call(sc);
 	if (ret.func_or_ret != OK) {
 		sbi_printf("cfn: copy wrong...");
@@ -83,18 +91,27 @@ static int sbi_ecall_trf_handler(unsigned long extid, unsigned long funcid,
 				 struct sbi_trap_regs *regs,
 				 struct sbi_ecall_return *out)
 {
+	struct secure_ret sret = { 0 };
+	long trf_error;
+
 	sbi_printf("[TRF] TRF Called with funcid: %lx\n", funcid);
-	sbi_printf("a0 = %lx, a1 = %lx, a2 = %lx\n", regs->a0, regs->a1,
-		   regs->a2);
+	sbi_printf("a0 = %lx, a1 = %lx, a2 = %lx, a3 = %lx, a4 = %lx, a5 = %lx\n",
+		   regs->a0, regs->a1, regs->a2, regs->a3, regs->a4, regs->a5);
 	struct trf_call sc = { .func_desc = funcid,
 			       .a0	  = regs->a0,
 			       .a1	  = regs->a1,
-			       .a2	  = regs->a2 };
+			       .a2	  = regs->a2,
+			       .a3	  = regs->a3,
+			       .a4	  = regs->a4,
+			       .a5	  = regs->a5 };
 	if (sc.func_desc == FUNC_SBI)
-		func_sbi(sc);
+		sret = func_sbi(sc);
 	else
-		secure_bridge(sc);
-	return 0;
+		sret = secure_bridge(sc);
+
+	trf_error = (long)sret.a0;
+	out->value = sret.a1;
+	return (int)trf_error;
 }
 
 struct sbi_ecall_extension ecall_trf;
@@ -102,7 +119,7 @@ struct sbi_ecall_extension ecall_trf;
 static int sbi_ecall_trf_register_extensions(void)
 {
 	struct trf_call sc = { .func_desc = INITIALIZE };
-	sbi_printf("[TRF] TRustForge RS registered at 0x%p\n",
+	sbi_printf("[TRF] TRustForge = 0x%p\n",
 		   _fw_start + RS_OFFSET);
 	secure_bridge(sc);
 	return sbi_ecall_register_extension(&ecall_trf);
